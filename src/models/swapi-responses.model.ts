7fa1cb2a94e38ef9f4c164e1b1ced08c3e102a48
Oliver Eisenbarth <oliver.eisenbarth@googlemail.com>
2019-02-21 22:06:40 +0100

    update model

--- src/models/swapi-responses.model.ts
+++ src/models/swapi-responses.model.ts
@@ -1,12 +1,13 @@
 export interface SwapiResponse {
   count: number;
   // results are paginated
-  next: URL | null;
-  previous: URL | null;
+  next: string | null;
+  previous: string | null;
   results: Result[];
 }
+export interface Result {}
 
-export interface Result {
+export interface Character extends Result {
   name: string;
   height: string;
   mass: string;
@@ -29,4 +30,39 @@ export enum Gender {
   Female = 'female',
   Male = 'male',
   NA = 'n/a'
+}
+
+export interface Film extends Result {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: string;
+  producer: string;
+  release_date: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export interface Species extends Result {
+  name: string;
+  classification: string;
+  designation: string;
+  average_height: string;
+  skin_colors: string;
+  hair_colors: string;
+  eye_colors: string;
+  average_lifespan: string;
+  homeworld: string;
+  language: string;
+  people: string[];
+  films: string[];
+  created: string;
+  edited: string;
+  url: string;
 }
